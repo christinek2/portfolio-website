a29373c263cc01842d503b8c0c65f2870036afd9
christinek2 <76640176+christinek2@users.noreply.github.com>
2021-01-20 16:59:28 -0800

    Add sticky scroll

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
-console.log("Hi there! Welcome to my Portfolio Site!");
+console.log('Hi there! Welcome to my Portfolio Site!')
+
 function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
-    x.className += ' responsive';
-  }
-  else {
-    x.className = 'navtoggle';
+    x.className += ' responsive'
+  } else {
+    x.className = 'navtoggle'
   }
 }
 
-let mainNavLinks = document.querySelectorAll("div ul li a");
-let mainSections = document.querySelectorAll("main section");
+let mainNavLinks = document.querySelectorAll('div ul li a')
+let mainSections = document.querySelectorAll('main section')
 
-let lastId;
-let cur = [];
+let lastId
+let cur = []
 
 // This should probably be throttled.
 // Especially because it triggers during smooth scrolling.
@@ -24,19 +24,19 @@ let cur = [];
 // });
 // Only not doing it here to keep this Pen dependency-free.
 
-window.addEventListener("scroll", event => {
-  let fromTop = window.scrollY;
+window.addEventListener('scroll', event => {
+  let fromTop = window.scrollY
 
   mainNavLinks.forEach(link => {
-    let section = document.querySelector(link.hash);
+    let section = document.querySelector(link.hash)
 
     if (
       section.offsetTop <= fromTop &&
       section.offsetTop + section.offsetHeight > fromTop
     ) {
-      link.classList.add("current");
+      link.classList.add('current')
     } else {
-      link.classList.remove("current");
+      link.classList.remove('current')
     }
-  });
-});
+  })
+})
